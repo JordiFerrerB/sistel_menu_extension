955b86a69ba78e9767fa26adb6f006348bb6af0f
Jordi Ferrer <jferrerbeltran@gmail.com>
2020-08-07 10:08:39 +0200

    Add wbfolder

--- menu_sistel_v2.js
+++ menu_sistel_v2.js
@@ -3,11 +3,7 @@ define(['qlik', 'jquery',
     'css!./src/css/main.css',
     'css!./src/css/scoped-bootstrap.css',
     'text!./src/partials/template.ng.html',
-<<<<<<< HEAD
-    './src/js/bootstrap.bundle.min'
-=======
     './src/js/bootstrap.min'
->>>>>>> 7963341ab8a4155c668074c98b1815a8ad6bcbf8
 ], 
 
 function(qlik, $, props, initProps, cssContent, bootstrapCSS ,htmlTemplate, bootstrapjs){
@@ -23,10 +19,6 @@ function(qlik, $, props, initProps, cssContent, bootstrapCSS ,htmlTemplate, boot
         controller: ['$scope', function ($scope) {
 
             $scope.initialize = function(){
-<<<<<<< HEAD
-                //Permite superponer el menú a otros objetos
-=======
->>>>>>> 7963341ab8a4155c668074c98b1815a8ad6bcbf8
                 //$(document.body).find(".qv-object-menu_sistel_v2").parent().parent().parent().parent().css("z-index","2147483629");
             }
 
@@ -39,13 +31,6 @@ function(qlik, $, props, initProps, cssContent, bootstrapCSS ,htmlTemplate, boot
                     'background-color': $scope.layout.color_fondo_resaltado.color + ' !important',
                     'color' :  event.target.style.color = $scope.layout.color_fuente_resaltada.color + ' !important'
                 };
-<<<<<<< HEAD
-                if(event.target.className.includes('hover-effected')){
-                    $(event.target).css(style);
-                }
-                $(document.body).find(".qv-object-menu_sistel_v2").parent().parent().parent().parent().css("z-index","2147483629");
-                //$(event.target).find('.nav-link, i').css(style);
-=======
 
                 //Add hover styles
                 if(event.target.className.includes('hover-effected')){
@@ -54,7 +39,6 @@ function(qlik, $, props, initProps, cssContent, bootstrapCSS ,htmlTemplate, boot
 
                 //Allow submenus to overlap other objects
                 $(document.body).find(".qv-object-menu_sistel_v2").parent().parent().parent().parent().css("z-index","2147483629");
->>>>>>> 7963341ab8a4155c668074c98b1815a8ad6bcbf8
             }
 
             $scope.hoverExitEvent = function(event){
@@ -62,19 +46,13 @@ function(qlik, $, props, initProps, cssContent, bootstrapCSS ,htmlTemplate, boot
                     'background-color': $scope.layout.color_fondo.color + ' !important',
                     'color' :  $scope.layout.color_fuente.color + ' !important'
                 }
-<<<<<<< HEAD
-=======
 
                 //Remove hover styles
->>>>>>> 7963341ab8a4155c668074c98b1815a8ad6bcbf8
                 if(event.target.className.includes('hover-effected')){
                     $(event.target).css(style);
                 }
 
-<<<<<<< HEAD
-=======
                 //Close submenu on mouseleave
->>>>>>> 7963341ab8a4155c668074c98b1815a8ad6bcbf8
                 if(event.target.className.includes('dropdown-toggle') && $(event.target).data('toggle') != 'dropdown'){
                     $(event.target).dropdown('toggle');
                 }
@@ -83,10 +61,7 @@ function(qlik, $, props, initProps, cssContent, bootstrapCSS ,htmlTemplate, boot
                     $('#'+ label).dropdown('toggle');
                 }
 
-<<<<<<< HEAD
-=======
                 //Prevent menu from overlapping unwanted objects
->>>>>>> 7963341ab8a4155c668074c98b1815a8ad6bcbf8
                 $(document.body).find(".qv-object-menu_sistel_v2").parent().parent().parent().parent().css("z-index","1");
             }
         }]
