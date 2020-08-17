b0aa07e2ff1e2bc3d537ca5ddf36a2c296c3c940
Jordi Ferrer <jferrerbeltran@gmail.com>
2020-08-17 08:35:22 +0200

    Add external URLs option

--- menu_sistel_v2.js
+++ menu_sistel_v2.js
@@ -1,69 +1,99 @@
-define(['qlik', 'jquery',
-    './src/properties', './src/initialproperties',
-    'css!./src/css/main.css',
-    'css!./src/css/scoped-bootstrap.css',
-    'text!./src/partials/template.ng.html',
-    './src/js/bootstrap.bundle.min'
-], 
+define([
+  "qlik",
+  "jquery",
+  "./src/properties",
+  "./src/initialproperties",
+  "css!./src/css/main.css",
+  "css!./src/css/scoped-bootstrap.css",
+  "text!./src/partials/template.ng.html",
+  "./src/js/bootstrap.bundle.min",
+], function (
+  qlik,
+  $,
+  props,
+  initProps,
+  cssContent,
+  bootstrapCSS,
+  htmlTemplate,
+  bootstrapjs
+) {
+  "use strict";
 
-function(qlik, $, props, initProps, cssContent, bootstrapCSS ,htmlTemplate, bootstrapjs){
-    'use strict';
+  $("<style>").html(cssContent).appendTo("head");
 
-    $( '<style>' ).html(cssContent).appendTo( 'head' );
+  return {
+    definition: props,
+    initialProperties: initProps,
+    template: htmlTemplate,
+    controller: [
+      "$scope",
+      function ($scope) {
+        $scope.initialize = function () {
+          //$(document.body).find(".qv-object-menu_sistel_v2").parent().parent().parent().parent().css("z-index","2147483629");
+        };
 
-    return {
-        definition: props,
-        initialProperties: initProps,
-        template: htmlTemplate,
-        controller: ['$scope', function ($scope) {
+        $scope.navigate = function (object) {
+          if (object.isLocalSheet) {
+            qlik.navigation.gotoSheet(object.hoja_destino);
+          } else {
+            window.open(object.url_destino);
+          }
+        };
 
-            $scope.initialize = function(){
-                //$(document.body).find(".qv-object-menu_sistel_v2").parent().parent().parent().parent().css("z-index","2147483629");
+        $scope.hoverEnterEvent = function (event) {
+          var style = {
+            "background-color":
+              $scope.layout.color_fondo_resaltado.color + " !important",
+            color: (event.target.style.color =
+              $scope.layout.color_fuente_resaltada.color + " !important"),
+          };
+
+          //Allow submenus to overlap other objects
+          $(document.body)
+            .find(".qv-object-menu_sistel_v2")
+            .parent()
+            .parent()
+            .parent()
+            .parent()
+            .css("z-index", "2147483629");
+
+          //Add hover styles
+          if (event.target.className.includes("hover-effected")) {
+            $(event.target).css(style);
+          }
+        };
+
+        $scope.hoverExitEvent = function (event) {
+          var style = {
+            "background-color": $scope.layout.color_fondo.color + " !important",
+            color: $scope.layout.color_fuente.color + " !important",
+          };
+
+          //Remove hover styles
+          if (event.target.className.includes("hover-effected")) {
+            $(event.target).css(style);
+          }
+
+          //Close submenu on mouseleave
+          if (event.target.className.includes("submenu-option")) {
+            var parentMenu = $(event.target).parent(".dropdown-menu");
+            var numHoverItems = parentMenu.find(".submenu-option:hover").length;
+
+            if (numHoverItems == 0) {
+              var label = parentMenu.attr("aria-labelledby");
+              $("#" + label).dropdown("toggle");
+              //Prevent menu from overlapping unwanted objects
+              $(document.body)
+                .find(".qv-object-menu_sistel_v2")
+                .parent()
+                .parent()
+                .parent()
+                .parent()
+                .css("z-index", "1");
             }
-
-            $scope.navigate = function(to){
-                qlik.navigation.gotoSheet(to);
-            }
-
-            $scope.hoverEnterEvent = function(event){
-                var style = {
-                    'background-color': $scope.layout.color_fondo_resaltado.color + ' !important',
-                    'color' :  event.target.style.color = $scope.layout.color_fuente_resaltada.color + ' !important'
-                };
-
-                //Add hover styles
-                if(event.target.className.includes('hover-effected')){
-                    $(event.target).css(style);
-                }
-
-                //Allow submenus to overlap other objects
-                $(document.body).find(".qv-object-menu_sistel_v2").parent().parent().parent().parent().css("z-index","2147483629");
-            }
-
-            $scope.hoverExitEvent = function(event){
-                var style = {
-                    'background-color': $scope.layout.color_fondo.color + ' !important',
-                    'color' :  $scope.layout.color_fuente.color + ' !important'
-                }
-
-                //Remove hover styles
-                if(event.target.className.includes('hover-effected')){
-                    $(event.target).css(style);
-                }
-
-                //Close submenu on mouseleave
-                if(event.target.className.includes('submenu-option')){
-                    var parentMenu = $(event.target).parent('.dropdown-menu');
-                    var numHoverItems = parentMenu.find('.submenu-option:hover').length;
-                    
-                    if(numHoverItems == 0){
-                        var label = parentMenu.attr('aria-labelledby');
-                        $('#' + label).dropdown('toggle');
-                        //Prevent menu from overlapping unwanted objects
-                        $(document.body).find(".qv-object-menu_sistel_v2").parent().parent().parent().parent().css("z-index","1");
-                    }
-                }
-            }
-        }]
-    }
+          }
+        };
+      },
+    ],
+  };
 });
--- src/properties.js
+++ src/properties.js
@@ -1,214 +1,280 @@
-define(['qlik', 'text!./data/lui-icons.json'], function(qlik, luiIcons){
-    'use strict';
+define(["qlik", "text!./data/lui-icons.json"], function (qlik, luiIcons) {
+  "use strict";
 
-    var sheets = [];
+  var sheets = [];
 
-    //Obtén todas las hojas del documento, sin repeticiones
-    function getSheets(){
-        sheets = [];
-        var app = qlik.currApp();
-        app.getAppObjectList("sheet", function ( data ) {      
-            data.qAppObjectList.qItems.forEach((s)=>{
-                var found = false;
-                //Impide que se añadan duplicados
-                for(var i = 0; i < sheets.length && !found; i++){
-                    if(s.qInfo.qId == sheets[i].value){
-                        found = true;
-                    }
-                }
-
-                if(!found){
-                    sheets.push({
-                        value: s.qInfo.qId,
-                        label: s.qMeta.title
-                    });
-                }
-            });
-        });
-    }
-
-    getSheets();
-
-    var menuColores = { 
-        type: "items",
-        ref: "colores",
-        label: "Colors",
-        items:  {
-            color_fondo: {
-                label: "Background color",
-                ref: "color_fondo",
-                type: "object",
-                component: "color-picker",
-                defaultValue: {
-                    color: "#009EE3",
-                    index: "-1"
-                }
-            },
-            color_fuente:{
-                label: "Font color",
-                ref: "color_fuente",
-                type: "object",
-                component: "color-picker",
-                defaultValue: {
-                    color: "#fffff",
-                    index: "-1"
-                }
-            },
-            color_resaltado: {
-                label: "Background hover color",
-                ref: "color_fondo_resaltado",
-                type: "object", 
-                component: "color-picker",
-                defaultValue: {
-                    color: "#999",
-                    index: "-1"
-                }
-            },
-            texto_resaltado: {
-                label: "Font hover color",
-                ref: "color_fuente_resaltada",
-                type: "object",
-                component: "color-picker",
-                defaultValue: {
-                    color: "#fffff",
-                    index: "-1"
-                }
-            }
+  //Obtén todas las hojas del documento, sin repeticiones
+  function getSheets() {
+    sheets = [];
+    var app = qlik.currApp();
+    app.getAppObjectList("sheet", function (data) {
+      data.qAppObjectList.qItems.forEach((s) => {
+        var found = false;
+        //Impide que se añadan duplicados
+        for (var i = 0; i < sheets.length && !found; i++) {
+          if (s.qInfo.qId == sheets[i].value) {
+            found = true;
+          }
         }
-    }
 
-    var addMenus = {
+        if (!found) {
+          sheets.push({
+            value: s.qInfo.qId,
+            label: s.qMeta.title,
+          });
+        }
+      });
+    });
+  }
+
+  getSheets();
+
+  var menuColores = {
+    type: "items",
+    ref: "colores",
+    label: "Colors",
+    items: {
+      color_fondo: {
+        label: "Background color",
+        ref: "color_fondo",
+        type: "object",
+        component: "color-picker",
+        defaultValue: {
+          color: "#009EE3",
+          index: "-1",
+        },
+      },
+      color_fuente: {
+        label: "Font color",
+        ref: "color_fuente",
+        type: "object",
+        component: "color-picker",
+        defaultValue: {
+          color: "#fffff",
+          index: "-1",
+        },
+      },
+      color_resaltado: {
+        label: "Background hover color",
+        ref: "color_fondo_resaltado",
+        type: "object",
+        component: "color-picker",
+        defaultValue: {
+          color: "#999",
+          index: "-1",
+        },
+      },
+      texto_resaltado: {
+        label: "Font hover color",
+        ref: "color_fuente_resaltada",
+        type: "object",
+        component: "color-picker",
+        defaultValue: {
+          color: "#fffff",
+          index: "-1",
+        },
+      },
+    },
+  };
+
+  var addMenus = {
+    type: "array",
+    label: "Menu links",
+    ref: "objetos_menu",
+    itemTitleRef: "titulo",
+    allowAdd: true,
+    addTranslation: "Add link",
+    allowRemove: true,
+    allowMove: true,
+    items: {
+      destino: {
+        type: "boolean",
+        label: "Destination",
+        ref: "isLocalSheet",
+        component: "switch",
+        options: [
+          {
+            label: "Sheet",
+            value: true,
+          },
+          {
+            label: "External URL",
+            value: false,
+          },
+        ],
+      },
+      titulo: {
+        type: "string",
+        label: "Title",
+        ref: "titulo",
+        expression: "optional",
+      },
+      icono: {
+        type: "string",
+        component: "dropdown",
+        label: "Icon",
+        ref: "icono",
+        options: function () {
+          var iconList = JSON.parse(luiIcons).icons;
+          var props = [
+            {
+              value: "",
+              label: ">> No icon <<",
+            },
+          ];
+
+          iconList.forEach(function (icon) {
+            props.push({
+              value: icon.id,
+              label: icon.name,
+            });
+          });
+
+          return props;
+        },
+      },
+      hojaDestino: {
+        type: "string",
+        component: "dropdown",
+        ref: "hoja_destino",
+        label: "Sheet",
+        options: function () {
+          return sheets;
+        },
+        show: function (menu) {
+          return menu.isLocalSheet && menu.objetos_submenu.length == 0;
+        },
+        defaultValue: "",
+      },
+      urlDestino: {
+        type: "string",
+        label: "URL",
+        ref: "url_destino",
+        defaultValue: "",
+        show: function (menu) {
+          return !menu.isLocalSheet && menu.objetos_submenu.length == 0;
+        },
+      },
+      visibilidad: {
+        type: "boolean",
+        component: "switch",
+        label: "Visible",
+        ref: "visible",
+        options: [
+          {
+            value: true,
+            label: "Yes",
+          },
+          {
+            value: false,
+            label: "No",
+          },
+        ],
+      },
+      submenus: {
         type: "array",
-        label: 'Menu links',
-        ref: 'objetos_menu',
-        itemTitleRef: "titulo",
+        ref: "objetos_submenu",
+        label: "Submenus",
+        itemTitleRef: "titulo_submenu",
         allowAdd: true,
-        addTranslation: 'Add link',
         allowRemove: true,
         allowMove: true,
+        addTranslation: "Add submenu",
         items: {
-            titulo: {
-                type: "string",
-                label: "Title",
-                ref: "titulo", 
-                expression: "optional"
-            },
-            icono: {
-                type: "string",
-                component: "dropdown",
-                label: "Icon",
-                ref: "icono",
-                options: function(){
-                    var iconList = JSON.parse(luiIcons).icons;
-                    var props = [{
-                        value: '',
-                        label: '>> No icon <<'
-                    }];
-
-                    iconList.forEach(function(icon){
-                        props.push({
-                            value: icon.id,
-                            label: icon.name
-                        });
-                    });
-
-                    return props;
-                }
-
-            },
-            destino: {
-                type: "string",
-                component: "dropdown",
-                ref: "hoja_destino",
+          titulo: {
+            type: "string",
+            label: "Submenu title",
+            ref: "titulo_submenu",
+            expression: "optional",
+          },
+          destino: {
+            type: "boolean",
+            label: "Destination",
+            ref: "isLocalSheet",
+            component: "switch",
+            options: [
+              {
                 label: "Sheet",
-                options: function(){return sheets;},
-                show: function(data){return data.objetos_submenu.length == 0},
-                defaultValue: ""
+                value: true,
+              },
+              {
+                label: "External URL",
+                value: false,
+              },
+            ],
+          },
+          hoja_destino: {
+            type: "string",
+            component: "dropdown",
+            ref: "hoja_destino",
+            label: "Sheet",
+            options: function () {
+              return sheets;
             },
-            visibilidad : {
-                type: "boolean",
-                component: "switch",
-                label: "Visible",
-                ref: "visible",
-                options: [{
-                    value: true,
-                    label: "Yes"
-                },{
-                    value: false,
-                    label: "No"
-                }]
+            defaultValue: "",
+            show: function (menu) {
+              return menu.isLocalSheet;
             },
-            submenus : {
-                type: "array",
-                ref: "objetos_submenu",
-                label: "Submenus",
-                itemTitleRef: "titulo_submenu",
-                allowAdd: true,
-                allowRemove: true,
-                allowMove: true,
-                addTranslation: 'Add submenu',
-                items: {
-                    titulo: {
-                        type: "string",
-                        label: "Submenu title",
-                        ref: "titulo_submenu", 
-                        expression: "optional"
-                    },
-                    destino: {
-                        type: "string",
-                        component: "dropdown",
-                        ref: "hoja_destino_submenu",
-                        label: "Sheet",
-                        options: function(){
-                            return sheets;
-                        },
-                        defaultValue: ""
-                    }
-                }
-            }
-        }
-    };
+          },
+          urlDestino: {
+            type: "string",
+            label: "URL",
+            ref: "url_destino",
+            defaultValue: "",
+            show: function (menu) {
+              return !menu.isLocalSheet;
+            },
+          },
+        },
+      },
+    },
+  };
 
-    var brandOptions = {
-        type: "items",
+  var brandOptions = {
+    type: "items",
+    label: "Brand",
+    ref: "logo",
+    items: {
+      toggle: {
+        type: "boolean",
+        component: "switch",
+        label: "Visible",
+        ref: "visibilidad_logo",
+        defaultValue: false,
+        options: [
+          {
+            value: true,
+            label: "Yes",
+          },
+          {
+            value: false,
+            label: "No",
+          },
+        ],
+      },
+      titulo: {
+        type: "string",
         label: "Brand",
-        ref: "logo",
-        items: {
-            toggle: {
-                type: "boolean",
-                component: "switch",
-                label: "Visible",
-                ref: "visibilidad_logo",
-                defaultValue: false,
-                options: [{
-                    value: true,
-                    label: "Yes"
-                },{
-                    value: false,
-                    label: "No"
-                }]
-            },
-            titulo : {
-                type: "string",
-                label: "Brand",
-                ref: "marca_menu",
-                show: function(data){return data.visibilidad_logo}
-            }
-        }
-    }
+        ref: "marca_menu",
+        show: function (data) {
+          return data.visibilidad_logo;
+        },
+      },
+    },
+  };
 
-    return {
-        type: 'items',
-        component: "accordion",
-        items:{
-            settings: {
-                uses: "settings",
-                items: { 
-                    colores: menuColores,
-                    logo: brandOptions,
-                    menus: addMenus
-                }
-            }
-        }
-    }
+  return {
+    type: "items",
+    component: "accordion",
+    items: {
+      settings: {
+        uses: "settings",
+        items: {
+          colores: menuColores,
+          logo: brandOptions,
+          menus: addMenus,
+        },
+      },
+    },
+  };
 });
